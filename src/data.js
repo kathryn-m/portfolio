b25a997d3d69c35d0251077220a18b8e6445f311
Kathryn M <72918908+kathryn-m@users.noreply.github.com>
2022-07-24 14:53:41 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,89 @@
+//Français
+
+export default [
+	{
+		id: 1,
+		role: "project",
+		title: "Jeu de cartes 'Memory'",
+		description: "Le jeu de cartes célèbre refait à neuf avec trois niveaux de difficulté et trois choix de nombre de cartes. En français ou en anglais! Seras-tu plus fort que l'ordinateur?",
+		proglang: `JavaScript CSS HTML`,
+		code: "https://github.com/kathryn-m/memory-game",
+		url: "https://kathryn-m.github.io/memory-game/",
+		image_webp: "/img/memory_fr.webp" ,
+		image: "/img/memory_fr.png" ,
+		alt: "Memory jeu de cartes"
+	},
+	{
+		id:2,
+		role: "project",
+		title: "Révisions Nature",
+		description: "Un site web publicitaire pour les séjours de préparation des concours en Normandie, France.",
+		proglang: "JavaScript CSS HTML",
+		code: "https://github.com/kathryn-m/revisions_nature",
+		url: "https://grand-camp.com/",
+		image_webp: "/img/revision.webp" ,
+		image: "/img/revision.png" ,
+		alt: "Site web pour les révisions nature"
+	},
+	{
+		id:3,
+		role: "project",
+		title: "Quizzical",
+		description: "Un quiz à choix multiples sur la culture générale. J'ai fait le code, mais le dessin était fourni en fichiers [https://www.figma.com]{Figma} par [https://scrimba.com] {Scrimba.} Le quiz utilise l'API [https://opentdb.com]{Open Trivia Database.}",
+		proglang: "React",
+		code: "https://github.com/kathryn-m/quizzical",
+		url: "https://quizzical.onrender.com",
+		image_webp: "/img/quizzical.webp" ,
+		image: "/img/quizzical.png",
+		alt: "quizzical quiz sur la culture générale"
+	},
+	{
+		id:4,
+		role: "component",
+		title: "Memories",
+		description: "Un carrousel d'images navigable par les touches flêches, les touches qui représentent chaque image, ou par l'autoplay, qui peut être démarré ou arrêté en cliquant sur l'image. Au début de la pandémie, ces images m'ont rapellé des temps meilleurs.",
+		proglang: "JavaScript CSS HTML",
+		code: "https://github.com/kathryn-m/memories-carousel",
+		url: "https://kathryn-m.github.io/memories-carousel/",
+		image_webp: "/img/memories.webp" ,
+		image: "/img/memories.png",
+		alt: "carrousel d'images nostalgiques des repères célèbres"
+	},
+	{
+		id:5,
+		role: "component",
+		title: "L'inspiration",
+		description: "Des citations sur plusieurs sujets, en seulement un clic. L'appli utilise l'API [https://pprathameshmore.github.io/QuoteGarden/]{QuoteGarden.}" ,
+		proglang: "JavaScript CSS HTML",
+		code: "https://github.com/kathryn-m/quote-app",
+		url: "https://kathryn-m.github.io/quote-app/",
+		image_webp: "/img/quotes.webp" ,
+		image: "/img/quotes.png",
+		alt: "l'appli pour trouver les citations inspirants"
+	},
+	{
+		id:6,
+		role: "component",
+		title: "Drôle de lapin",
+		description: "Un lapin avec les oreilles, les yeux et le nez qui bougent.",
+		proglang: "JavaScript CSS HTML",
+		code: "https://github.com/kathryn-m/funny-bunny",
+		url: "https://kathryn-m.github.io/funny-bunny/",
+		image_webp: "/img/rabbit.webp" ,
+		image: "/img/rabbit.png",
+		alt: "Une animation d'un lapin joyeux." ,
+	},
+	{
+		id:7,
+		role: "project",
+		title: "Port-feuille",
+		description: "Des exemples des projets entièrs et des composants par Kathryn M." ,
+		proglang: "React",
+		code: "https://github.com/kathryn-m/portfolio",
+		url: "#",
+		image_webp: "/img/portfolio.webp" ,
+		image: "/img/portfolio.png",
+		alt: "Le site web port-feuille que vous naviguez actuellement"
+	},
+
+]
