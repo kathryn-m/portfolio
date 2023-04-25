3490bacf5fa065df389c816abe1e59a07acf0d2a
kathryn-m <72918908+kathryn-m@users.noreply.github.com>
2023-04-25 18:07:52 +0100

    feat2: changed site from French to English

--- src/data.js
+++ src/data.js
@@ -1,89 +1,89 @@
-//Français
+//English
 
 export default [
 	{
 		id: 1,
 		role: "project",
-		title: "Jeu de cartes 'Memory'",
-		description: "Le jeu de cartes célèbre refait à neuf avec trois niveaux de difficulté et trois choix de nombre de cartes. En français ou en anglais! Seras-tu plus fort que l'ordinateur?",
+		title: "Memory card game",
+		description: "The famous card game re-created with three levels of difficulty and three deck sizes. Your choice of English or French. Can you beat the computer?",
 		proglang: `JavaScript CSS HTML`,
 		code: "https://github.com/kathryn-m/memory-game",
 		url: "https://kathryn-m.github.io/memory-game/",
-		image_webp: "/img/memory_fr.webp" ,
-		image: "/img/memory_fr.png" ,
-		alt: "Memory jeu de cartes"
+		image_webp: "/img/memory_en.webp" ,
+		image: "/img/memory_en.png" ,
+		alt: "memory card game"
 	},
 	{
 		id:2,
 		role: "project",
 		title: "Révisions Nature",
-		description: "Un site web publicitaire pour les séjours de préparation des concours en Normandie, France.",
+		description: "A website for a study retreat in Normandy, France.",
 		proglang: "JavaScript CSS HTML",
 		code: "https://github.com/kathryn-m/revisions_nature",
-		url: "https://revisions-nature.onrender.com",
+		url: "https://grand-camp.com/",
 		image_webp: "/img/revision.webp" ,
 		image: "/img/revision.png" ,
-		alt: "Site web pour les révisions nature"
+		alt: "révisions nature website"
 	},
 	{
 		id:3,
 		role: "project",
 		title: "Quizzical",
-		description: "Un quiz à choix multiples sur la culture générale. J'ai fait le code, mais le dessin était fourni en fichiers [https://www.figma.com]{Figma} par [https://scrimba.com] {Scrimba.} Le quiz utilise l'API [https://opentdb.com]{Open Trivia Database.}",
+		description: "A mulitple-choice general knowledge quiz. The code is my own, but the design was supplied in [https://www.figma.com]{Figma} files by [https://scrimba.com]{Scrimba.} The quiz uses the [https://opentdb.com]{Open Trivia Database} API.",
 		proglang: "React",
 		code: "https://github.com/kathryn-m/quizzical",
 		url: "https://quizzical.onrender.com",
 		image_webp: "/img/quizzical.webp" ,
 		image: "/img/quizzical.png",
-		alt: "quizzical quiz sur la culture générale"
+		alt: "quizzical general knowledge quiz"
 	},
 	{
 		id:4,
 		role: "component",
 		title: "Memories",
-		description: "Un carrousel d'images navigable par les touches flêches, les touches qui représentent chaque image, ou par l'autoplay, qui peut être démarré ou arrêté en cliquant sur l'image. Au début de la pandémie, ces images m'ont rapellé des temps meilleurs.",
+		description: "A photo carousel navigable with the arrow buttons, the buttons representing each photo, or by autoplay, which can be started or stopped by clicking on the photo. At the beginning of the pandemic these photos reflected my nostalgia for better times.",
 		proglang: "JavaScript CSS HTML",
 		code: "https://github.com/kathryn-m/memories-carousel",
 		url: "https://kathryn-m.github.io/memories-carousel/",
 		image_webp: "/img/memories.webp" ,
 		image: "/img/memories.png",
-		alt: "carrousel d'images nostalgiques des repères célèbres"
+		alt: "nostalgic photo carousel of famous landmarks"
 	},
 	// {
 	// 	id:5,
 	// 	role: "component",
-	// 	title: "L'inspiration",
-	// 	description: "Des citations sur plusieurs sujets, en seulement un clic. L'appli utilise l'API [https://pprathameshmore.github.io/QuoteGarden/]{QuoteGarden.}" ,
+	// 	title: "A pop of inspiration",
+	// 	description: "Quotes on many topics, at the click of a button. The app uses the [https://pprathameshmore.github.io/QuoteGarden/]{QuoteGarden} API." ,
 	// 	proglang: "JavaScript CSS HTML",
 	// 	code: "https://github.com/kathryn-m/quote-app",
 	// 	url: "https://kathryn-m.github.io/quote-app/",
 	// 	image_webp: "/img/quotes.webp" ,
 	// 	image: "/img/quotes.png",
-	// 	alt: "l'appli pour trouver les citations inspirants"
+	// 	alt: "quote-finding application"
 	// },
 	{
 		id:6,
 		role: "component",
-		title: "Drôle de lapin",
-		description: "Un lapin avec les oreilles, les yeux et le nez qui bougent.",
+		title: "Funny bunny",
+		description: "Light-hearted rabbit animation." ,
 		proglang: "JavaScript CSS HTML",
 		code: "https://github.com/kathryn-m/funny-bunny",
 		url: "https://kathryn-m.github.io/funny-bunny/",
 		image_webp: "/img/rabbit.webp" ,
 		image: "/img/rabbit.png",
-		alt: "Une animation d'un lapin joyeux." ,
+		alt: "A rabbit with animated eyes and ears"
 	},
 	{
 		id:7,
 		role: "project",
 		title: "Portfolio",
-		description: "Des exemples des projets entièrs et des composants par Kathryn M." ,
+		description: "Examples of complete projects and components by Kathryn M." ,
 		proglang: "React",
 		code: "https://github.com/kathryn-m/portfolio",
 		url: "#",
-		image_webp: "/img/portfolio_fr2.webp" ,
-		image: "/img/portfolio_fr2.png",
-		alt: "Le site web portfolio que vous naviguez actuellement"
+		image_webp: "/img/portfolio_en.webp" ,
+		image: "/img/portfolio_en.png",
+		alt: "The portfolio website you are currently browsing"
 	},
 
 ]
